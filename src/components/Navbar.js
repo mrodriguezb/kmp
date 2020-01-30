0dd75e4d2fafe5063cdf790db01d8a491c2d7d8c
Merche <mercedes.rodriguez@movicoders.com>
2020-01-30 15:56:02 +0100

    Login screen
    login screen implemented

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class Navbar extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+        </div>
+        </div>
+        </nav>);
+    }
+}
+
+export default Navbar;
